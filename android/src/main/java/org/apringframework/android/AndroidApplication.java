758bf2c6236668ae8af5b151c36db932ccd57439
Singlerr <singlerr@soongsil.ac.kr>
2022-09-10 21:12:45 +0900

    Constructing base

---
+++ android/src/main/java/org/apringframework/android/AndroidApplication.java
@@ -0,0 +1,31 @@
+package org.apringframework.android;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apringframework.ApringApplication;
+
+/***
+ * Entrypoint of apring framework.
+ * By overriding {@link ApringApplication#onApplicationStart()} and {@link ApringApplication#onApplicationStop()}, it notifies start/stop of application.
+ * @see ApringApplication
+ * @author Singlerr
+ */
+public abstract class AndroidApplication extends AppCompatActivity implements ApringApplication{
+    /***
+     * DO NOT OVERRIDE THIS METHOD!
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onApplicationStart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        onApplicationStop();
+    }
+}
